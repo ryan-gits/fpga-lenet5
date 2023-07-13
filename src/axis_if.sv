8104ba6d833da03688fecb6196082b3f93ad9cb1
Ryan Robertson <rrobertson@gmail.com>
2023-07-13 10:39:05 -0700

    add axi stream sv interface

---
+++ src/axis_if.sv
@@ -0,0 +1,35 @@
+interface axis_if #(parameter DATA_WIDTH = 8) (
+  logic clock,
+  logic reset
+);
+
+logic [DATA_WIDTH-1:0]         tdata;
+logic                          tvalid;
+logic                          tready;
+logic                          tkeep;
+logic [$clog2(DATA_WIDTH)-1:0] tstrb;
+logic                          tlast;
+
+modport main (
+  input  clock,
+  input  reset,
+  output tdata,
+  output tvalid,
+  input  tready,
+  output tkeep,
+  output tstrb,
+  output tlast
+);
+
+modport peripheral (
+  input  clock,
+  input  reset,
+  input  tdata,
+  input  tvalid,
+  output tready,
+  input  tkeep,
+  input  tstrb,
+  input  tlast
+);
+
+endinterface
